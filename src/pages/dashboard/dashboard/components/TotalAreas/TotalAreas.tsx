85d4d142da398b82e8540c177eb7e2f54536992a
Felipe.L <felipeluciano19@gmail.com>
2023-09-19 21:41:08 -0300

    🎉 feat(TotalAreas.tsx): add TotalAreas component to display total areas information in the dashboard 🔀 chore(index.ts): export TotalAreas component from dashboard components index file 🔄 refactor(index.tsx): replace existing code for displaying total areas with TotalAreas component in the Dashboard component

---
+++ src/pages/dashboard/dashboard/components/TotalAreas/TotalAreas.tsx
@@ -0,0 +1,37 @@
+import { Card, CardBody, Flex, Heading, Box, Text } from '@chakra-ui/react'
+import * as Icons from '../../../../../components/Icons/Icons.tsx'
+
+export const TotalAreas = () => {
+    return (
+        <Card mt={{ base: 80, md: 40 }} p={8}>
+            <CardBody>
+                <Flex gap={6} direction={{ base: 'column', md: 'row' }} alignItems={{ base: 'center', md: 'flex-start' }}>
+                    <Heading fontSize={'lg'} mr={{ base: 0, md: '16' }} mb={{ base: 5, md: 0 }}>Áreas Totais</Heading>
+                    <Box width={110} bg={'gray.200'} borderRadius={'md'} p={'3'} position={'relative'} minH={59}>
+                        <Icons.IconArea w={'30px'} h={'29px'} mixBlendMode={'multiply'} position={'absolute'} top={-4} left={'50%'} transform={'translateX(-50%)'} />
+                        <Text fontWeight={'bold'} color={'gray.600'}>437.1 ha.</Text>
+                        <Text color={'gray.500'} fontSize={'sm'}>TOTAL</Text>
+                    </Box>
+
+                    <Box width={110} bg={'gray.200'} borderRadius={'md'} p={'3'} position={'relative'} minH={59}>
+                        <Icons.IconArea w={'30px'} h={'29px'} mixBlendMode={'multiply'} position={'absolute'} top={-4} left={'50%'} transform={'translateX(-50%)'} />
+                        <Text fontWeight={'bold'} color={'gray.600'}>287.3 ha.</Text>
+                        <Text color={'gray.500'} fontSize={'sm'}>ARRENDADA</Text>
+                    </Box>
+                    <Box width={110} bg={'gray.200'} borderRadius={'md'} p={'3'} position={'relative'} minH={59}>
+                        <Icons.IconArea w={'30px'} h={'29px'} mixBlendMode={'multiply'} position={'absolute'} top={-4} left={'50%'} transform={'translateX(-50%)'} />
+                        <Text fontWeight={'bold'} color={'gray.600'}>380.6 ha.</Text>
+                        <Text color={'gray.500'} fontSize={'sm'}>PRÓPRIA</Text>
+                    </Box>
+                    <Box width={110} bg={'gray.200'} borderRadius={'md'} p={'3'} position={'relative'} minH={59}>
+                        <Icons.IconArea w={'30px'} h={'29px'} mixBlendMode={'multiply'} position={'absolute'} top={-4} left={'50%'} transform={'translateX(-50%)'} />
+                        <Text fontWeight={'bold'} color={'gray.600'}>352.2 ha.</Text>
+                        <Text color={'gray.500'} fontSize={'sm'}>PRODUTIVA</Text>
+                    </Box>
+                </Flex>
+            </CardBody>
+        </Card>
+    )
+}
+
+export default TotalAreas
--- src/pages/dashboard/dashboard/index.tsx
+++ src/pages/dashboard/dashboard/index.tsx
@@ -1,24 +1,13 @@
 import React from 'react'
 import { Container, Card, CardBody, Flex, Heading, Box, Text  } from '@chakra-ui/react'
-import {  Header } from './components'
+import {  Header, TotalAreas } from './components'
 import * as Icons from '../../../components/Icons/Icons.tsx'
 
 export const Dashboard = (): React.ReactElement => {
     return (
         <Container minW={'100%'} position={'relative'}>
            <Header/>
-           <Card mt={{base: 80, md: 40}} p={8}>
-            <CardBody>
-                <Flex>
-                    <Heading fontSize={'lg'} mr={'16'}>Áreas Totais</Heading>
-                    <Box width={110} bg={'gray.200'} borderRadius={'md'} p={'3'} position={'relative'} minH={59}>
-                        <Icons.IconArea w={'30px'} h={'29px'} mixBlendMode={'multiply'} position={'absolute'} top={-4} left={'50%'} transform={'translateX(-50%)'}/>
-                        <Text fontWeight={'bold'} color={'gray.600'}>437.1 ha.</Text>
-                        <Text color={'gray.500'} fontSize={'sm'}>TOTAL</Text>
-                    </Box>
-                </Flex>
-            </CardBody>
-           </Card>
+           <TotalAreas/>
         </Container>
     )
 }
