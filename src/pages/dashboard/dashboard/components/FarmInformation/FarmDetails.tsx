04a3de1be1275a90d12e5f5fd3c4121941aa3082
Felipe.L <felipeluciano19@gmail.com>
2023-09-22 14:33:45 -0300

    📦 chore(package.json): add @react-google-maps/api package as a dependency 🆕 feat(FarmDetails.tsx): create a new component for displaying farm details 🔄 refactor(FarmInformationSection.tsx): import and use FarmDetails component in FarmInformationSection 🔄 refactor(index.ts): export FarmDetails component from components/index.ts

---
+++ src/pages/dashboard/dashboard/components/FarmInformation/FarmDetails.tsx
@@ -0,0 +1,31 @@
+import {
+    Flex, Box, Text, Divider,
+} from '@chakra-ui/react'
+import { CheckCircleIcon } from '@chakra-ui/icons'
+
+export const FarmDetails = () => {
+    return (
+        <>
+            <Flex direction={'column'} gap={3}>
+                <Box rounded={'md'} bg={'gray.200'} p={2} alignSelf={'flex-start'}>
+                    <Text color={'gray.600'} fontSize={'sm'}>Média Propriedade Produtiva</Text>
+                </Box>
+                <Text color={'gray.400'} maxW={'398px'}>ROD BR 359 REGIÃO DO POSTO DA PÓLVORA + 6 KM ESQUERDA</Text>
+                <Text color={'gray.600'} fontWeight={'bold'}>ALCINÓPOLIS - MS</Text>
+                <Divider />
+            </Flex>
+            <Flex direction={'column'} gap={3} mt={3}>
+                <Text color={'gray.400'}>NIRF</Text>
+                <Text color={'gray.600'} fontSize={'lg'} fontWeight={'bold'}>-</Text>
+                <Text color={'gray.400'}>INCRA</Text>
+                <Flex gap={3} alignItems={'center'}>
+                    <Text color={'gray.600'} fontSize={'lg'} fontWeight={'bold'}>9017166180987</Text>
+                    <CheckCircleIcon fontSize={25} color={'green.400'} />
+                    <Text color={'green.400'} fontWeight={'bold'}>Quitado</Text>
+                </Flex>
+            </Flex>
+        </>
+    )
+}
+
+export default FarmDetails
--- src/pages/dashboard/dashboard/components/FarmInformation/FarmInformationSection.tsx
+++ src/pages/dashboard/dashboard/components/FarmInformation/FarmInformationSection.tsx
@@ -1,66 +1,101 @@
 import {
-    Flex, Heading, Box, Text, Divider,
-    TableContainer, Th, Table, Thead, Tbody, Tr, Td,
-    Tfoot
+  Flex, Heading, Box, Text, Divider,
+  TableContainer, Th, Table, Thead, Tbody, Tr, Td,
+  Tfoot
 } from '@chakra-ui/react'
 import { CheckCircleIcon } from '@chakra-ui/icons'
-import { BoxSection, BoxArea, Tablerize } from './../../components'
+
+import { BoxSection, BoxArea, Tablerize, FarmDetails } from './../../components'
 import * as Icons from '../../../../../components/Icons/Icons.tsx'
 
-export const FarmInformationSection = () => {
-    return (
-        <BoxSection title={'Fazenda Salto Alto'} mt={6} textAlign={'left'}>
-            <Flex direction={'column'} width={'40%'}>
-                <Flex direction={'column'} gap={3}>
-                    <Box rounded={'md'} bg={'gray.200'} p={2} alignSelf={'flex-start'}>
-                        <Text color={'gray.600'} fontSize={'sm'}>Média Propriedade Produtiva</Text>
-                    </Box>
-                    <Text color={'gray.400'} maxW={'398px'}>ROD BR 359 REGIÃO DO POSTO DA PÓLVORA + 6 KM ESQUERDA</Text>
-                    <Text color={'gray.600'} fontWeight={'bold'}>ALCINÓPOLIS - MS</Text>
-                    <Divider />
-                </Flex>
-                <Flex direction={'column'} gap={3} mt={3}>
-                    <Text color={'gray.400'}>NIRF</Text>
-                    <Text color={'gray.600'} fontSize={'lg'} fontWeight={'bold'}>-</Text>
-                    <Text color={'gray.400'}>INCRA</Text>
-                    <Flex gap={3} alignItems={'center'}>
-                        <Text color={'gray.600'} fontSize={'lg'} fontWeight={'bold'}>9017166180987</Text>
-                        <CheckCircleIcon fontSize={25} color={'green.400'} />
-                        <Text color={'green.400'} fontWeight={'bold'}>Quitado</Text>
-                    </Flex>
-                </Flex>
-                <Flex direction={'column'} mt={10}>
-                    <Tablerize
-                        title="MATRÍCULAS"
-                        headers={['Num.', 'Área', 'UF', 'Município']}
-                        rows={[
-                            ['12102', '233.2', 'MS', 'Coxim'],
-                            ['1102', '23.2', 'SP', 'Braganca Paulista'],
-                        ]}
-                    />
-                </Flex>
+import React, { useState, useRef, useCallback } from 'react'
+import { GoogleMap, useJsApiLoader, Polygon } from '@react-google-maps/api';
 
-                <Flex direction={'column'} mt={10}>
-                    <Tablerize
-                        title="PROPRIETÁRIOS"
-                        headers={['Nome.', '%']}
-                        rows={[
-                            ['FERNANDO C.','12.3'],
-                            ['CARLOS L.', '87.7'],
-                        ]}
-                    />
-                </Flex>
-                
-                <Flex direction={'column'} mt={10} gap={6}>
-                    <Heading fontSize={'lg'} mr={{ base: 0, md: '16' }} mb={3} color={'gray.400'}>ÁREAS</Heading>
-                    <Flex gap={3}>
-                        <BoxArea />
-                        <BoxArea />
-                    </Flex>
-                </Flex>
+const containerStyle = {
+  width: '100%',
+  height: '400px'
+};
+
+const center = {
+  lat: 52.513281130944264, lng: 13.36346611492683
+};
+
+
+export const FarmInformationSection = () => {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: ""
+  })
+
+  const path = [
+    { lat: 52.513281130944264, lng: 13.36346611492683 },
+    { lat: 52.512575957821824, lng: 13.362994046140209 },
+    { lat: 52.512445368964684, lng: 13.363637776303783 },
+    { lat: 52.513124426783875, lng: 13.364088387418285 }
+  ];
+
+  const polygonOptions = {
+    paths: path,
+    strokeColor: '#FF0000',
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillOpacity: 0.35,
+  };
+
+  return (
+    <BoxSection title={'Fazenda Salto Alto'} mt={6} textAlign={'left'}>
+      <Flex>
+        <Flex direction={'column'} width={'40%'}>
+          <FarmDetails />
+          <Flex direction={'column'} mt={10}>
+            <Tablerize
+              title="MATRÍCULAS"
+              headers={['Num.', 'Área', 'UF', 'Município']}
+              rows={[
+                ['12102', '233.2', 'MS', 'Coxim'],
+                ['1102', '23.2', 'SP', 'Braganca Paulista'],
+              ]}
+            />
+          </Flex>
+
+          <Flex direction={'column'} mt={10}>
+            <Tablerize
+              title="PROPRIETÁRIOS"
+              headers={['Nome.', '%']}
+              rows={[
+                ['FERNANDO C.', '12.3'],
+                ['CARLOS L.', '87.7'],
+              ]}
+            />
+          </Flex>
+
+          <Flex direction={'column'} mt={10} gap={6}>
+            <Heading fontSize={'lg'} mr={{ base: 0, md: '16' }} mb={3} color={'gray.400'}>ÁREAS</Heading>
+            <Flex gap={3}>
+              <BoxArea />
+              <BoxArea />
             </Flex>
-        </BoxSection>
-    )
+          </Flex>
+        </Flex>
+        <Flex direction={'column'} width={'60%'} bg={'gray.100'} ml={6}>
+          {isLoaded ? (
+            <GoogleMap
+              options={{mapTypeId: 'satellite', disableDefaultUI: true}}
+              mapContainerClassName='App-map'
+              mapContainerStyle={containerStyle}
+              center={center}
+              zoom={17}
+            >
+              <Polygon
+                options={polygonOptions}
+                path={path}
+              />
+            </GoogleMap>
+          ) : <></>}
+        </Flex>
+      </Flex>
+    </BoxSection>
+  )
 }
 
 export default FarmInformationSection
