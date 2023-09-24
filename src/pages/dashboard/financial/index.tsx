e20f6c2f3c653f0dc8b8fcdfe71369576a6c4521
Felipe.L <felipeluciano19@gmail.com>
2023-09-24 05:38:27 -0300

    🔨 refactor(financial): remove unnecessary empty lines and closing tag to improve code readability

--- src/pages/dashboard/financial/index.tsx
+++ src/pages/dashboard/financial/index.tsx
@@ -5,10 +5,7 @@ import { BoxSection, BoxArea } from '../../../components/'
 
 export const Financial = (): React.ReactElement =>{
 
-    return (<BoxSection mt={{ base: 80, md: 40 }} pt={6}>
-
-
-    </BoxSection>)
+    return (<BoxSection mt={{ base: 80, md: 40 }} pt={6}><></></BoxSection>)
 }
 
 export default Financial
